487e95fd296dbd1216b0cf8e90c296d115454a45
Justin Louie <22231868+JustinMYeung@users.noreply.github.com>
2023-07-12 09:33:35 -0400

    Create Graphs for Data Analysis
    20-100%

--- cycleAnalysisCompilation.m
+++ cycleAnalysisCompilation.m
@@ -54,32 +54,51 @@ detrendAvgs = mean(diffDetrend, 3); % calculate mean of each test at each cycle 
 detrendMins = min(diffDetrend, [], 3); % calculate min of each test at each cycle and flux percentage
 detrendMaxs = max(diffDetrend, [], 3); % calculate max of each test at each cycle and flux percentage
 
-% TODO: Abstract and put into for loop for 20 40, 60, 80, 100
-% probably 2:10:2
+for i = 2:2:10 % loop through to create graphs of the 20%, 40%, 60%, 80%, and 100% graphs
+    figure()
 
-figure()
-subplot(2, 2, 1)
+    percent = i * 10;
+    sgtitle(percent + "% Flux Avg Comparisons"); % Create title of overall Figure
 
-col = 10;
-
-plot(cycles, interpAvgs(:, col), 'b', 'LineWidth', 2.0); hold on;
-fill([cycles; flipud(cycles)], [interpMins(:, col); flipud(interpMaxs(:, col))], 'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
-xlabel("Cycle")
-ylabel("Percent Difference")
-title("Interpolated Flux Difference")
-
-subplot(2, 2, 2)
-plot(cycles, normAvgs(:, col), 'b', 'LineWidth', 2.0); hold on;
-fill([cycles; flipud(cycles)], [normMins(:, col); flipud(normMaxs(:, col))], 'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
-xlabel("Cycle")
-ylabel("Percent Difference")
-title("Normalized Flux Difference")
-
-subplot(2, 2, 3)
-plot(cycles, detrendAvgs(:, col), 'b', 'LineWidth', 2.0); hold on;
-fill([cycles; flipud(cycles)], [detrendMins(:, col); flipud(detrendMaxs(:, 10))], 'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
-xlabel("Cycle")
-ylabel("Percent Difference")
-title("Detrended Flux Difference")
+    % Interpolated Diff Subplot
+    subplot(2, 2, 1)
+    yLowerLimInterp = min(interpAvgs(:, i) * 1.5 - 20);
+    yUpperLimInterp = max(interpAvgs(:, i) * 1.5 + 20);
 
 
+    plot(cycles, interpAvgs(:, i), 'b', 'LineWidth', 2.0); hold on;
+    fill([cycles; flipud(cycles)], [interpMins(:, i); flipud(interpMaxs(:, i))], 'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
+    % ylim([yLowerLimInterp yUpperLimInterp]);
+    ylim([-50 50])
+    xlabel("Cycle");
+    ylabel("Percent Difference");
+    title("Interpolated Flux Difference");
+
+    % Normalized Diff Subplot
+    subplot(2, 2, 2)
+    yLowerLimNorm = min(normAvgs(:, i) * 1.5 - 20);
+    yUpperLimNorm = max(normAvgs(:, i) * 1.5 + 20);
+    plot(cycles, normAvgs(:, i), 'b', 'LineWidth', 2.0); hold on;
+    fill([cycles; flipud(cycles)], [normMins(:, i); flipud(normMaxs(:, i))], 'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
+    % ylim([yLowerLimNorm yUpperLimNorm]);
+    ylim([-50 50])
+    xlabel("Cycle");
+    ylabel("Percent Difference");
+    title("Normalized Flux Difference");
+
+    % Detrended Diff Subplot
+    subplot(2, 2, 3)
+    yLowerLimDetrend = min(detrendAvgs(:, i) * 1.5 - 20);
+    yUpperLimDetrend = max(detrendAvgs(:, i) * 1.5 + 20);
+    plot(cycles, detrendAvgs(:, i), 'b', 'LineWidth', 2.0); hold on;
+    fill([cycles; flipud(cycles)], [detrendMins(:, i); flipud(detrendMaxs(:, 10))], 'b', 'FaceAlpha', 0.2, 'EdgeColor', 'none');
+    % ylim([yLowerLimDetrend yUpperLimDetrend])
+    ylim([-50 50])
+    xlabel("Cycle")
+    ylabel("Percent Difference")
+    title("Detrended Flux Difference")
+end
+
+
+
+% TODO: scale the detrended to relevant region (0 - 50)
